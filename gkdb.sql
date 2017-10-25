4151bbdbe04c4d301a481d37529f825d82a33fa2
wasuradananjith <wasuradananjith@gmail.com>
2017-10-25 22:31:55 +0530

    db updated

--- gkdb.sql
+++ gkdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 25, 2017 at 03:32 PM
+-- Generation Time: Oct 25, 2017 at 05:01 PM
 -- Server version: 5.7.9
 -- PHP Version: 5.6.16
 
@@ -19,6 +19,127 @@ SET time_zone = "+00:00";
 --
 -- Database: `gkdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `appointment_id` varchar(20) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `start_time` varchar(11) NOT NULL,
+  `end_time` varchar(11) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `cust_id` varchar(20) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`appointment_id`,`cust_id`),
+  KEY `fk_Appointment_Customer1` (`cust_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appointment_id`, `appointment_date`, `start_time`, `end_time`, `description`, `cust_id`, `status`) VALUES
+('APP0000001', '2017-10-07', '1500', '1600', 'Wedding', 'REG0000001', 'rejected'),
+('APP0000002', '2017-10-07', '1700', '1800', 'Wedding', 'REG0000001', 'accepted'),
+('APP0000003', '2017-10-07', '1400', '1430', 'Wedding', 'REG0000001', 'accepted'),
+('APP0000004', '2017-10-07', '1900', '2030', 'Wedding', 'REG0000001', 'accepted'),
+('APP0000005', '2017-10-07', '0100', '0200', 'Birthday', 'REG0000001', 'rejected'),
+('APP0000006', '2017-10-11', '1300', '1400', 'Wedding', 'REG0000001', 'accepted'),
+('APP0000007', '2017-10-17', '1300', '1400', 'Birthday', 'REG0000001', 'accepted'),
+('APP0000008', '2017-10-20', '0100', '0200', 'Wedding', 'REG0000001', 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_request`
+--
+
+DROP TABLE IF EXISTS `appointment_request`;
+CREATE TABLE IF NOT EXISTS `appointment_request` (
+  `request_id` varchar(20) NOT NULL,
+  `appointment_date` date NOT NULL,
+  `start_time` varchar(11) NOT NULL,
+  `end_time` varchar(11) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `cust_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`request_id`,`cust_id`),
+  KEY `fk_Appointment_Customer1` (`cust_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `cust_id` varchar(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `cust_phone` varchar(12) NOT NULL,
+  `cust_address` varchar(60) NOT NULL,
+  `cust_email` varchar(30) NOT NULL,
+  `date_joined` date NOT NULL,
+  `password` varchar(40) NOT NULL,
+  PRIMARY KEY (`cust_id`),
+  UNIQUE KEY ` cust_phone_UNIQUE` (`cust_phone`),
+  UNIQUE KEY `cust_email_UNIQUE` (`cust_email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cust_id`, `first_name`, `last_name`, `cust_phone`, `cust_address`, `cust_email`, `date_joined`, `password`) VALUES
+('CUS0000004', 'Peter', 'Pan', '0778965478', 'Colombo', 'peter@gmail.com', '0000-00-00', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'),
+('REG0000001', 'Vishni', 'Ganepola', '0775896548', 'Kandana', 'vishni@gmail.com', '2017-09-01', '900150983cd24fb0d6963f7d28e17f72'),
+('REG0000002', 'Ama', 'Gamage', '0714562389', 'Wadduwa', 'wasurawattearachchi@gmail.com', '2017-09-09', '900150983cd24fb0d6963f7d28e17f72'),
+('REG0000003', 'Peter', 'Pan', '0775478965', 'Moratuwa', 'wasuradananjith@gmail.com', '2017-09-25', '900150983cd24fb0d6963f7d28e17f72');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
+  `emp_id` varchar(20) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `emp_email` varchar(50) NOT NULL,
+  `emp_phone` varchar(12) NOT NULL,
+  `emp_address` varchar(60) NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `emp_type` varchar(20) DEFAULT NULL,
+  `emp_gender` varchar(10) NOT NULL,
+  `is_user` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`emp_id`),
+  UNIQUE KEY `emp_email_UNIQUE` (`emp_email`),
+  UNIQUE KEY `emp_phone_UNIQUE` (`emp_phone`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`emp_id`, `first_name`, `last_name`, `emp_email`, `emp_phone`, `emp_address`, `username`, `password`, `emp_type`, `emp_gender`, `is_user`) VALUES
+('EMP0000001', 'Wasura', 'Wattearachchi', 'wasuradananjith@gmail.com', '0775706398', 'Moratuwa', NULL, NULL, 'Administrator', 'Male', 1),
+('EMP0000002', 'Thilakshika', 'Udyani', 'thilakshika@gmail.com', '0771236547', 'Panadura', NULL, NULL, 'Receptionist', 'Female', 1),
+('EMP0000003', 'Dharana', 'Weerawarna', 'wdharana@gmail.com', '0714589656', 'Moratuwa', NULL, NULL, 'Beautician', 'Male', 0),
+('EMP0000004', 'Elankumaran', 'Thanga', 'elankumaran@gmail.com', '0774565456', 'Jaffna', NULL, NULL, 'Administrator', 'Male', 0),
+('EMP0000005', 'Avishka', 'Perera', 'avishka@gmail.com', '0774589653', 'Rathmalana', NULL, NULL, 'Beautician', 'Male', 0),
+('EMP0000006', 'Sachini', 'Fernando', 'sachini@gmail.com', '0714589652', 'Rajagiriya', NULL, NULL, 'Beautician', 'Female', 0),
+('EMP0000007', 'Surangi', 'De Silva', 'surangi@gmail.com', '0778965412', 'Moratuwa', NULL, NULL, 'Beautician', 'Female', 0),
+('EMP0000008', 'Mohammed', 'Imdad', 'imdad@gmail.com', '0789655412', 'Jaffna', NULL, NULL, 'Beautician', 'Male', 0);
 
 -- --------------------------------------------------------
 
