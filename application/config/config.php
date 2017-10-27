626097ea51eea5d9572093ce7d970ddc1875e624
wasuradananjith <wasuradananjith@gmail.com>
2017-10-27 10:31:48 +0530

    base url corrected

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/myphp/GK-Photography/';
+$config['base_url'] = 'http://localhost/GK-Photography/';
 
 /*
 |--------------------------------------------------------------------------
